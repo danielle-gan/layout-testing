1e3870f3435512a8ae1f799a82e14ded89998e06
Danielle Gan <DGan@data-mail.com>
2023-03-07 12:51:45 -0500

    finally fixed the flex box and added javascript functionality

---
+++ script.js
@@ -0,0 +1,53 @@
+
+$(function () {
+    $('#desktop').on('click', function () {
+        $(".delete-on-click").remove();
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click").appendTo('#testing1');
+        $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click").appendTo('#testing2');
+        $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click").appendTo('#testing3');
+        $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing3');
+        console.log("clicked");
+    });
+});
+
+
+$(function () {
+    $('#home').on('click', function () {
+        $(".delete-on-click").remove();
+        $('<i>').addClass("fa fa-home fa-3x delete-on-click").appendTo('#testing1');
+        $('<p>home</p>').addClass("delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-home fa-3x delete-on-click").appendTo('#testing2');
+        $('<p>home</p>').addClass("delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-home fa-3x delete-on-click").appendTo('#testing3');
+        $('<p>home</p>').addClass("delete-on-click").appendTo('#testing3');
+        console.log("clicked");
+    });
+});
+
+$(function () {
+    $('#documents').on('click', function () {
+        $(".delete-on-click").remove();
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing1');
+        $('<p>file1</p>').addClass("delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing2');
+        $('<p>file2</p>').addClass("delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing3');
+        $('<p>file3</p>').addClass("delete-on-click").appendTo('#testing3');
+        console.log("clicked");
+    });
+});
+
+$(function () {
+    $('#downloads').on('click', function () {
+        $(".delete-on-click").remove();
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing1');
+        $('<p>file4</p>').addClass("delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing2');
+        $('<p>file5</p>').addClass("delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing3');
+        $('<p>file6</p>').addClass("delete-on-click").appendTo('#testing3');
+        console.log("clicked");
+    });
+});
