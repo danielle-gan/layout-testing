b4f3a2d3ce8e2a157f4eef7b5825c7a45ce47549
Danielle Gan <DGan@data-mail.com>
2023-03-07 15:49:02 -0500

    more layout tinkering

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 $(function () {
     $('#desktop').on('click', function () {
         $(".delete-on-click").remove();
-        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click pt-3").appendTo('#testing1');
         $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing1');
-        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click pt-3").appendTo('#testing2');
         $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing2');
-        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click").appendTo('#testing3');
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click pt-3").appendTo('#testing3');
+        $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing3');
+        console.log("clicked");
+    });
+});
+
+$(function () {
+    $('#desktop').on('click', function () {
+        $(".delete-on-click").remove();
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click pt-3").appendTo('#testing1');
+        $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click pt-3").appendTo('#testing2');
+        $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-desktop fa-3x delete-on-click pt-3").appendTo('#testing3');
         $('<p>desktop</p>').addClass("delete-on-click").appendTo('#testing3');
         console.log("clicked");
     });
@@ -16,11 +29,11 @@ $(function () {
 $(function () {
     $('#home').on('click', function () {
         $(".delete-on-click").remove();
-        $('<i>').addClass("fa fa-home fa-3x delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-home fa-3x delete-on-click pt-3").appendTo('#testing1');
         $('<p>home</p>').addClass("delete-on-click").appendTo('#testing1');
-        $('<i>').addClass("fa fa-home fa-3x delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-home fa-3x delete-on-click pt-3").appendTo('#testing2');
         $('<p>home</p>').addClass("delete-on-click").appendTo('#testing2');
-        $('<i>').addClass("fa fa-home fa-3x delete-on-click").appendTo('#testing3');
+        $('<i>').addClass("fa fa-home fa-3x delete-on-click pt-3").appendTo('#testing3');
         $('<p>home</p>').addClass("delete-on-click").appendTo('#testing3');
         console.log("clicked");
     });
@@ -29,11 +42,11 @@ $(function () {
 $(function () {
     $('#documents').on('click', function () {
         $(".delete-on-click").remove();
-        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click pt-3").appendTo('#testing1');
         $('<p>file1</p>').addClass("delete-on-click").appendTo('#testing1');
-        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click pt-3").appendTo('#testing2');
         $('<p>file2</p>').addClass("delete-on-click").appendTo('#testing2');
-        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing3');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click pt-3").appendTo('#testing3');
         $('<p>file3</p>').addClass("delete-on-click").appendTo('#testing3');
         console.log("clicked");
     });
@@ -42,11 +55,11 @@ $(function () {
 $(function () {
     $('#downloads').on('click', function () {
         $(".delete-on-click").remove();
-        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing1');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click pt-3").appendTo('#testing1');
         $('<p>file4</p>').addClass("delete-on-click").appendTo('#testing1');
-        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing2');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click pt-3").appendTo('#testing2');
         $('<p>file5</p>').addClass("delete-on-click").appendTo('#testing2');
-        $('<i>').addClass("fa fa-file fa-3x delete-on-click").appendTo('#testing3');
+        $('<i>').addClass("fa fa-file fa-3x delete-on-click pt-3").appendTo('#testing3');
         $('<p>file6</p>').addClass("delete-on-click").appendTo('#testing3');
         console.log("clicked");
     });
